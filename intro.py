ab47a0fcfab8215a9a15ba989e424f0f513b5f23
LivinkumarStackly <livinkumar@thestackly.com>
2026-07-06 11:57:14 +0530

    Introduction

---
+++ intro.py
@@ -0,0 +1,17 @@
+# these are printing statements.
+
+'''print(55)
+print(44)
+print(33)'''
+
+# print('hello')
+
+# print(12.0)
+
+# print("Hello world 123&%*&^%&^$")
+
+# print(True)
+
+# print(type(False))  #<class 'int'>
+
+# print(input("Enter Your age: "))
