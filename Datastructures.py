07e97900f6b526db9aa45e7d0c2bc72353a4fb5d
niranjan122005 <niranjanv@thestackly.com>
2026-07-24 11:15:24 +0530

    Datastructures - set, dict

--- Datastructures.py
+++ Datastructures.py
@@ -53,3 +53,65 @@ t1=(1,2,3,3,2,1,"Hello")
 # name,age,role=person
 
 # print(age)
+
+
+# Set 
+
+# s1={4,5,6,7,8}
+# s2={1,2,3,4,5,4,3,2,1}
+# s1.add(999)
+# s1.remove(56)
+# s1.clear()
+# print(s1)
+# print(s1.union(s2))
+# print(s1.intersection(s2))
+# print(s1.difference(s2))
+# print(s2.difference(s1))
+# print(s1&s2)
+# print(s1*s2)
+
+# Dict 
+
+# person1={
+#     "name":"Ken",
+#     "age":23,
+#     "Role":"SASE",
+#     "isMarried":False,
+#     "isPresent":False,
+# }
+
+# person1["isPresent"]=True
+# person1["favMovies"]=["24","I"]
+
+# person1.pop("isMarried")
+# person1.clear()
+
+# print(person1.keys())
+# print(person1.values())
+# print(person1.items())
+
+# for i in {1,2,3,4,4,3,2,1}:
+#     print(i)
+
+# for i in person1:
+#     print(person1[i])
+
+
+# l1=[
+#     1,
+#     2,
+#     3,
+#     [
+#         "four",
+#         "five",
+#         "six",
+#         [
+#             "seven",
+#             "eight",
+#             "nine"
+#         ]
+#     ]
+# ]
+
+# # print(l1[3][3][2])
+# print(l1[-1][-1][-1][-1])
